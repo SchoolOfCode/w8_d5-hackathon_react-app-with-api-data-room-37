7aa1129e4328a335b0b7ff8daa8425db7288df32
Chirag Mehta <chiragmehta.uk@gmail.com>
2022-01-14 11:19:12 +0000

    Hard coded 3 days of temp

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,41 +1,41 @@
-import './App.css';
+import "./App.css";
 
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-import Input from '../Input'
-import WeatherResult from '../Weather_Result';
-
+import Input from "../Input";
+import WeatherResult from "../Weather_Result";
 
 function App() {
+  const [userInput, setuserInput] = useState("");
+  const [weatherData, setweatherData] = useState([]);
 
-const [userInput, setuserInput] = useState('')
-const [weatherData, setweatherData] = useState([])
+  useEffect(() => {
+    async function getWeather() {
+      // check that user entered input
+      if (userInput === "") {
+        return;
+      }
 
-useEffect(() => {
-  async function getWeather() {
-    // check that user entered input
-    if (userInput === "") {
-      return
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&units=metric&cnt=3&appid=160cec49564aa8277fc523ac242c7bb0`
+      );
+
+      const data = await res.json();
+      console.log(data);
+
+      setweatherData({
+        name: data.city.name,
+        temp1: `${Math.round(Number(data.list[0].main.temp))}°C`,
+        temp2: `${Math.round(Number(data.list[1].main.temp))}°C`,
+        temp3: `${Math.round(Number(data.list[2].main.temp))}°C`,
+      });
     }
+    getWeather();
+  }, [userInput]);
 
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&units=metric&cnt=1&appid=160cec49564aa8277fc523ac242c7bb0`
-    );
-
-
-    const data = await res.json();
-    console.log(data)
-    
-    setweatherData({
-      name: data.city.name,
-      temp: `${Math.round(Number(data.list[0].main.temp))}°C`,
-    })
+  function handleSubmit(text) {
+    setuserInput(text);
   }
-  getWeather()
-}, [userInput])
-
-function handleSubmit(text) {
-  setuserInput(text)
-}
 
   return (
     <div className="App">
--- src/Components/Weather_Result/index.js
+++ src/Components/Weather_Result/index.js
@@ -4,7 +4,9 @@ const WeatherResult = ({weatherData}) => {
     return (
         <div>
           <h2>{weatherData.name}</h2>
-          <p>{weatherData.temp}</p>
+          <p>Todays Weather: {weatherData.temp1}</p>
+          <p>Tomorrow's Weather: {weatherData.temp2}</p>
+          <p>Two days time: {weatherData.temp3}</p>
         </div>
     )
 }
