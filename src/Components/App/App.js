f59b48a7e6f8e4ed89854f13523d6123a0383c54
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-01-14 13:39:44 +0000

    multiple-page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,6 +8,24 @@ import WeatherResult from "../Weather_Result";
 function App() {
   const [userInput, setuserInput] = useState("");
   const [weatherData, setweatherData] = useState([]);
+  const [daysRequested, setDaysRequested] = useState(3)
+
+  function day() { 
+    const day = new Date();
+    const week = new Array(
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday")
+    ;
+    for ( let i = 0; i < daysRequested; i++) {
+   return week[(day.getDay() + i) % 7];
+    }
+  }
+ 
 
   useEffect(() => {
     async function getWeather() {
@@ -17,18 +35,22 @@ function App() {
       }
 
       const res = await fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&units=metric&cnt=3&appid=160cec49564aa8277fc523ac242c7bb0`
+        `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&units=metric&cnt=${daysRequested}&appid=160cec49564aa8277fc523ac242c7bb0`
       );
 
       const data = await res.json();
       console.log(data);
 
       setweatherData({
-        name: data.city.name,
-        temp1: `${Math.round(Number(data.list[0].main.temp))}°C`,
-        temp2: `${Math.round(Number(data.list[1].main.temp))}°C`,
-        temp3: `${Math.round(Number(data.list[2].main.temp))}°C`,
+        //name: data.city.name,
+        // temp1: `${Math.round(Number(data.list[0].main.temp))}°C`,
+        tempArray: data.list.map(function(eachTemp){
+          return eachTemp.main.temp
+        })
+        // temp2: `${Math.round(Number(data.list[1].main.temp))}°C`,
+        // temp3: `${Math.round(Number(data.list[2].main.temp))}°C`,
       });
+      console.log(weatherData)
     }
     getWeather();
   }, [userInput]);
@@ -37,11 +59,15 @@ function App() {
     setuserInput(text);
   }
 
+{/* <ul>{highlights.map(function (item){
+      return <li>{item}</li>
+    })}</ul> */}
+
   return (
     <div className="App">
       <h1>Weather Finder</h1>
       <Input handleSubmit={handleSubmit} />
-      <WeatherResult weatherData={weatherData} />
+      <WeatherResult weatherData={weatherData} day={day}/>
     </div>
   );
 }
--- src/Components/Weather_Result/index.js
+++ src/Components/Weather_Result/index.js
@@ -1,14 +1,30 @@
 import React from 'react'
 
-const WeatherResult = ({weatherData}) => {
+const WeatherResult = ({weatherData, day}) => {
     return (
         <div>
           <h2>{weatherData.name}</h2>
-          <p>Todays Weather: {weatherData.temp1}</p>
+          {/* <p>{day} {weatherData.temp1}</p>
           <p>Tomorrow's Weather: {weatherData.temp2}</p>
-          <p>Two days time: {weatherData.temp3}</p>
+          <p>Two days time: {weatherData.temp3}</p> */}
+          {/* <ol>
+            {
+              weatherData.tempArray.map(function(eachTemp)
+              {
+                return <p>
+                  {(eachTemp.main.temp)}
+                </p>
+              })
+            }
+          </ol> */}
         </div>
     )
 }
 
 export default WeatherResult
+
+{/* <ul>{highlights.map(function (item){
+      return <li>{item}</li>
+    })}</ul> */}
+
+    // `${Math.round(Number(data.list[0].main.temp))}°C`
